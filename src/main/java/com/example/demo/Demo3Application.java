af346551ce1c7d6664439c9e7191ceb289a0b356
Chen Feng <cfcz48@126.com>
2022-10-17 16:05:38 +0800

    add

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.net.*;
 import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 public class Demo3Application implements CommandLineRunner {
@@ -22,18 +23,24 @@ public class Demo3Application implements CommandLineRunner {
         if("1".equals(next)) {
             System.out.println("请输入监听的端口号：");
             int port = scanner.nextInt();
-            DatagramSocket mSocket = new DatagramSocket(port);
+            ReceiverThread t = new ReceiverThread(port);
+            new Thread(t).start();
             while (true) {
-                byte[] receiveData = new byte[1024];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                mSocket.receive(receivePacket);// 在接收到信息之前，一直保持阻塞状态
-                System.out.println("客户端说:" + new String(receiveData).trim());
+                System.out.println("请输入需要发生的信息：");
+                String message = scanner.next();
+                InetSocketAddress address = new InetSocketAddress(t.getAddress().getHostAddress() ,65525);
+                byte[] messageBytes = message.getBytes("utf8");
+                DatagramPacket sendPacket = new DatagramPacket(messageBytes, messageBytes.length, address);
+                DatagramSocket datagramSocket = new DatagramSocket();
+                datagramSocket.send(sendPacket);
             }
         }else {
             System.out.println("请输入服务器IP地址：");
             String ip = scanner.next();
             System.out.println("请输入监听的端口号：");
             int port = scanner.nextInt();
+            ReceiverThread t = new ReceiverThread(65525);
+            new Thread(t).start();
             while (true) {
                 System.out.println("请输入需要发生的信息：");
                 String message = scanner.next();
